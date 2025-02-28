ef7ff29fd7931c7c9fff484309d8984523e7ca8b
thuong <hoaithuong889904@gmail.com>
2025-02-28 14:44:45 +0700

    finally

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,49 @@ window.addEventListener('DOMContentLoaded',function(e){
     navClose.onclick = function(e){
         document.body.classList.remove('show-menu');
     }
-})
+});
+//
+
+$(document).ready(function () {
+    $('#slickslider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      autoplayspeed: 3000,
+      prevArrow: $('#prev'),
+      nextArrow: $('#next'),
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            dots: true,
+          },
+        },
+      ],
+    });
+  });
+//
+window.addEventListener('DOMContentLoaded',function(e){
+  const ulink= document.querySelector(".up-link a");
+  ulink.onclick=function (e) {
+    e.preventDefault();
+      if(document.body.classList.contains('show-all')) {
+        document.body.classList.remove('show-all');
+        ulink.textContent="See all upcoming events ";
+      }
+      else{
+        document.body.classList.add('show-all');
+        ulink.textContent="show less";
+      }  
+  };
+});
+
